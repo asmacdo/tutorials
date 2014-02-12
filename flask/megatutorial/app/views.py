4feb60f9366d3e7b751ddb64dd8f66eabbe0177a
Austin <asmacdo@gmail.com>
2014-02-12 11:48:32 -0500

    6, user can edit profile

--- flask/megatutorial/app/views.py
+++ flask/megatutorial/app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User, ROLE_USER, ROLE_ADMIN
 from app import app, db, lm, oid
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -54,14 +55,36 @@ def user(nickname):
                            user = user,
                            posts = posts)
 
+@app.route('/edit', methods = ['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html',
+                           form = form)
+
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 @app.before_request
 def before_request():
-    print "before request"
     g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
+
 
 @oid.after_login
 def after_login(resp):
