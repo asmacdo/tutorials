e89d3667bf5dfe262028fe0d243d3b84b64f4b7c
Austin <asmacdo@gmail.com>
2014-02-13 17:07:39 -0500

    6.5, altered shebang to be local to specific env

--- flask/megatutorial/app/__init__.py
+++ flask/megatutorial/app/__init__.py
@@ -35,3 +35,6 @@ if not app.debug:
     from logging.handlers import RotatingFileHandler
     file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024, 10)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
